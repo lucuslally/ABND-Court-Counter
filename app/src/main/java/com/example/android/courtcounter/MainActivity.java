2790a728697a32164b5a24c1677b80847935440d
Lucus Lally <lucuslally1@gmail.com>
2018-08-11 14:45:14 -0700

    Adding styling and finishing up look and feel

--- app/src/main/java/com/example/android/courtcounter/MainActivity.java
+++ app/src/main/java/com/example/android/courtcounter/MainActivity.java
@@ -7,30 +7,64 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    int scoreTeamA = 0;
+    int scoreTeamB = 0;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-
     }
-        /**
-         * Displays the given score for Team A.
-         */
-        public void displayForTeamA(int score) {
-            TextView scoreView = (TextView) findViewById(R.id.team_a_score);
-            scoreView.setText(String.valueOf(score));
-        }
 
-        public void addThreeForTeamA (View view) {
-            displayForTeamA(3);
-        }
-        public void addTwoForTeamA (View view) {
-            displayForTeamA(2);
-        }
-        public void addOneForTeamA (View view) {
-            displayForTeamA(1);
-        }
-
+    /**
+     * Displays the given score for Team A.
+     */
+    public void displayForTeamA(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_a_score);
+        scoreView.setText(String.valueOf(score));
     }
+
+    public void displayForTeamB(int score) {
+        TextView scoreView = (TextView) findViewById(R.id.team_b_score);
+        scoreView.setText(String.valueOf(score));
+    }
+
+    public void resetTeamScores(View view) {
+        scoreTeamA = 0;
+        displayForTeamA(0);
+        scoreTeamB = 0;
+        displayForTeamB(0);
+    }
+
+    public void addThreeForTeamA(View view) {
+        scoreTeamA = scoreTeamA + 3;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addTwoForTeamA(View view) {
+        scoreTeamA = scoreTeamA + 2;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addOneForTeamA(View view) {
+        scoreTeamA = scoreTeamA + 1;
+        displayForTeamA(scoreTeamA);
+    }
+
+    public void addThreeForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 3;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addTwoForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 2;
+        displayForTeamB(scoreTeamB);
+    }
+
+    public void addOneForTeamB(View view) {
+        scoreTeamB = scoreTeamB + 1;
+        displayForTeamB(scoreTeamB);
+    }
+}
 
